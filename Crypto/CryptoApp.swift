11320b8492e72b21e5ae085cf86e6267a2ad3d5d
Giovane <giovane.barreira@arctouch.com>
2022-08-30 07:20:46 -0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Giovane Barreira on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
